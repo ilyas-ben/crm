7dcd82be9ec8d919855c79c77ddd793e1c44d539
ilyas <ilyasben313@gmail.com>
2024-06-20 10:36:37 +0100

    added image property

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -5,6 +5,7 @@ namespace App\Entity;
 use App\Repository\UserRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
+use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity(repositoryClass: UserRepository::class)]
@@ -36,6 +37,9 @@ class User
      */
     #[ORM\OneToMany(targetEntity: Profile::class, mappedBy: 'usser')]
     private Collection $profile;
+
+    #[ORM\Column(type: Types::TEXT, nullable: true)]
+    private ?string $image = null;
 
     public function __construct()
     {
@@ -133,6 +137,18 @@ class User
                 $profile->setUsser(null);
             }
         }
+
+        return $this;
+    }
+
+    public function getImage(): ?string
+    {
+        return $this->image;
+    }
+
+    public function setImage(string $image): static
+    {
+        $this->image = $image;
 
         return $this;
     }
